dc617e86dc9b2dac72c33dfccf0d57148ba35519
Michael Corcias <michael.corcias@gelt.com>
2025-04-18 17:32:36 +0200

    put y delete

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware')
-const { crearTarea, obtenerTareas } = require('../controllers/taskController')
+const { crearTarea, obtenerTareas, editarTarea, eliminarTarea } = require('../controllers/taskController')
 
-
+// Ruta TASK para crear tarea
 router.post('/', authMiddleware, crearTarea)
 
+// Ruta TASK para obtener tarea
 router.get('/', authMiddleware, obtenerTareas)
 
+// Ruta TASK para editar tarea
+router.put('/:id', authMiddleware, editarTarea)
+
+// Ruta TASK para elininar tarea
+router.delete('/:id', authMiddleware, eliminarTarea)
 
 module.exports = router;
